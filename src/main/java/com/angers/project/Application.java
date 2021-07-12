ecc13d769bb94fc6e200f8ead552b89811790926
elevenanger <elevenangery@gmail.com>
2021-07-12 10:03:13 +0800

    添加.gitignore

--- src/main/java/com/angers/project/Application.java
+++ src/main/java/com/angers/project/Application.java
@@ -1,7 +1,6 @@
 package com.angers.project;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.web.bind.annotation.RequestMapping;
